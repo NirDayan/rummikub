d30d0bbd7c782af70b7c56b62405794df6abbe1b
Lior <Lior@10.0.0.6>
2015-10-30 21:37:20 +0200

    Added Unit Test Class and some basic rummikub methods

--- Rummikub/src/logic/TilesDeck.java
+++ Rummikub/src/logic/TilesDeck.java
@@ -1,5 +1,30 @@
 package logic;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class TilesDeck {
-    
+
+    public static final int LOWEST_TILE_VALUE = 1;
+    public static final int HIGHEST_TILE_VALUE = 13;
+    public static final int NUMBER_OF_JOKERS = 2;
+    private ArrayList<Tile> deck;
+
+    public TilesDeck() {
+        createAllTilesInDeck();
+        Collections.shuffle(deck);
+    }
+
+    private void createAllTilesInDeck() {
+        //Iterate all tile types and add 2 of each type
+        for (Color color : Color.values()) {
+            for (int value = LOWEST_TILE_VALUE; value <= HIGHEST_TILE_VALUE; value++) {
+                deck.add(new Tile(color, value));
+                deck.add(new Tile(color, value));
+            }
+        }
+        deck.add(new Tile(Color.Joker));
+        deck.add(new Tile(Color.Joker));
+    }
+
 }
