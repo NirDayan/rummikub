cc4e0c7212c34d18d43eeb43d5c56c499305d6f2
Nir Dayan <nir.dayan@sap.com>
2015-11-02 19:41:10 +0200

    Move "user logic" from GamesManager into Game

--- Rummikub/src/logic/Game.java
+++ Rummikub/src/logic/Game.java
@@ -1,17 +1,47 @@
 package logic;
 
 import java.util.ArrayList;
+import controllers.IController;
 
 public class Game {
     
-    private ArrayList<Player> players;
+    private static ArrayList<Player> allPlayers = new ArrayList<Player>();
+    private ArrayList<Player> gamePlayers;
     private TilesDeck tilesDeck;
     private Board board;
     private Player currentPlayer;
+    private IController controller;
+    private static int nextPlayerID = 1;
+    private final int MAX_PLAYERS_NUM = 4;
+    private final String COMPUTER_NAME_PREFIX = "Computer#";
     
-    public Game() {
-        this.players = new ArrayList<Player>();
+    public static enum Status {
+        WAIT,
+        ACTIVE
+    };
+    
+    public Game(IController controller) {
+        this.gamePlayers = new ArrayList<Player>();
         this.currentPlayer = null;
+        this.controller = controller;
+    }
+    
+    public Game init() {
+        Game result = null;
+        boolean isGameInitialized = false;
+        
+        while (!isGameInitialized) {
+            GameDetails initialUserInput = controller.getInitialGameInput();
+            if (isGameInputValid(initialUserInput)) {
+                createPlayers(initialUserInput);
+                isGameInitialized = true;
+            }
+            else {
+                controller.showWrongInitialGameInput();
+            }
+        }        
+        
+        return result;
     }
     
     public TilesSequence createSequence (int playerID, Tile[] tiles) {
@@ -39,6 +69,63 @@ public class Game {
         if (currentPlayer == null) {
             currentPlayer = player;            
         }
-        players.add(player);
+        allPlayers.add(player);
+        gamePlayers.add(player);
+    }
+    
+    //TODO: more edge cases???
+    //TODO: check load from file flow.. currently it unhandeled
+    private boolean isGameInputValid (GameDetails input) {
+        String [] playerNames = input.getPlayersNames();
+                
+        if (input.getTotalPlayersNumber() < 2 || input.getTotalPlayersNumber() > MAX_PLAYERS_NUM)
+            return false;
+        if (playerNames.length < input.getHumenPlayersNum())
+            return false;        
+        //check names validity and each name is unique
+        for (int i = 0; i < input.getHumenPlayersNum(); i++) {
+            if (playerNames[i].isEmpty())
+                return false;
+            if (playerNames[i].startsWith(COMPUTER_NAME_PREFIX))
+                return false;
+            //check names are unique
+            for (int j = i + 1; j < input.getHumenPlayersNum(); j++) {
+                if (playerNames[i] == playerNames[j]) {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+    
+    private void createPlayers(GameDetails gameDetails) {
+        int currPlayerID;
+        String currPlayerName;
+        Player currPlayer;
+        int humanPlayersNum = gameDetails.getHumenPlayersNum();
+        int computerPlayerIndex = 1;
+        
+        for (int i = 0; i < gameDetails.getTotalPlayersNumber(); i++) {
+            currPlayerID = generatePlayerId();
+            //first create human players
+            if (humanPlayersNum > 0) {
+                currPlayerName = gameDetails.getPlayersNames()[i];
+                currPlayer = new HumanPlayer(currPlayerID, currPlayerName);
+                humanPlayersNum--;                
+            }
+            else {
+                //create computer player
+                currPlayerName = COMPUTER_NAME_PREFIX + computerPlayerIndex;
+                currPlayer = new ComputerPlayer(currPlayerID, currPlayerName);
+                computerPlayerIndex++;
+            }
+            
+            addPlayer(currPlayer);
+        }        
+    }
+    
+    private int generatePlayerId() {
+        return (nextPlayerID)++;
     }
 }
--- Rummikub/src/logic/GamesManager.java
+++ Rummikub/src/logic/GamesManager.java
@@ -5,43 +5,26 @@ import java.util.ArrayList;
 import java.util.Map;
 
 public class GamesManager {
-    
-    private final int FIRST_PLAYER_ID = 1;
-    private final int MAX_PLAYERS_NUM = 4;
-    private final String COMPUTER_NAME_PREFIX = "Computer#";
+
     private final IController controller;
     private Map<Game, ArrayList<GameAction>> gamesActions;
     private ArrayList<Game> games;
-    private ArrayList<Player> players;
-    private int nextPlayerId;
     
     public GamesManager (IController controller) {
-        this.players = new ArrayList<Player>();
         this.games = new ArrayList<Game>();
         this.controller = controller;
-        this.nextPlayerId = FIRST_PLAYER_ID;
     }
     
-    public void start() {
-        boolean isGameInitialized = false;
-        while (!isGameInitialized) {
-            GameDetails initialUserInput = controller.getInitialGameInput();
-            if (isGameInputValid(initialUserInput)){
-                createGame(initialUserInput);
-                isGameInitialized = true;
-            }
-            else {
-                controller.showWrongInitialGameInput();
-            }
-        }
+    public void start() {       
+        Game game = new Game(controller);
+        game.init();
+        games.add(game);
         
         manageGames();
     }
     
     public void createGame (GameDetails gameDetails) {        
-        Game game = new Game();
-        createPlayers(gameDetails, game);
-        games.add(game);
+
     }
     
     public String createGameFromXML (String xmlData) {
@@ -73,65 +56,8 @@ public class GamesManager {
     }
     
     public void manageGames() {
-        for (Game game : games) {
-            controller.renderGame(game);            
-        }
-    }
-    
-    private void createPlayers(GameDetails gameDetails, Game game) {
-        int currPlayerID;
-        String currPlayerName;
-        Player currPlayer;
-        int humanPlayersNum = gameDetails.getHumenPlayersNum();
-        int computerPlayerIndex = 1;
-        
-        for (int i = 0; i < gameDetails.getTotalPlayersNumber(); i++) {
-            currPlayerID = generatePlayerId();
-            //first create human players
-            if (humanPlayersNum > 0) {
-                currPlayerName = gameDetails.getPlayersNames()[i];
-                currPlayer = new HumanPlayer(currPlayerID, currPlayerName);
-                humanPlayersNum--;                
-            }
-            else {
-                //create computer player
-                currPlayerName = COMPUTER_NAME_PREFIX + computerPlayerIndex;
-                currPlayer = new ComputerPlayer(currPlayerID, currPlayerName);
-                computerPlayerIndex++;
-            }
+        for(Game game : games) {
             
-            players.add(currPlayer);
-            game.addPlayer(currPlayer);//add the current player into the game
-        }        
-    }
-    
-    private int generatePlayerId() {
-        return (nextPlayerId)++;
-    }
-    
-    //TODO: more edge cases???
-    //TODO: check load from file flow.. currently it unhandeled
-    public boolean isGameInputValid (GameDetails input) {
-        String [] playerNames = input.getPlayersNames();
-                
-        if (input.getTotalPlayersNumber() < 2 || input.getTotalPlayersNumber() > MAX_PLAYERS_NUM)
-            return false;
-        if (playerNames.length < input.getHumenPlayersNum())
-            return false;        
-        //check names validity and each name is unique
-        for (int i = 0; i < input.getHumenPlayersNum(); i++) {
-            if (playerNames[i].isEmpty())
-                return false;
-            if (playerNames[i].startsWith(COMPUTER_NAME_PREFIX))
-                return false;
-            //check names are unique
-            for (int j = i + 1; j < input.getHumenPlayersNum(); j++) {
-                if (playerNames[i] == playerNames[j]) {
-                    return false;
-                }
-            }
         }
-        
-        return true;
-    }
+    }    
 }
