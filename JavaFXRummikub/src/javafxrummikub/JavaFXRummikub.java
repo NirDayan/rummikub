db3e65157260cf5dac5f6eaaf78a2b446143064c
Lior <Lior@Lior-LapTop>
2015-12-16 08:56:50 +0200

    New Game Scene is created every time main menu button is pressed

--- JavaFXRummikub/src/javafxrummikub/JavaFXRummikub.java
+++ JavaFXRummikub/src/javafxrummikub/JavaFXRummikub.java
@@ -2,8 +2,6 @@ package javafxrummikub;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -11,6 +9,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafxrummikub.scenes.gameplay.GamePlaySceneController;
 import javafxrummikub.scenes.newGame.NewGameSceneController;
+import logic.Game;
 
 public class JavaFXRummikub extends Application {
     private static final String GAME_PLAY_SCENE_FILE_PATH = "/javafxrummikub/scenes/gameplay/GamePlayScene.fxml";
@@ -18,13 +17,15 @@ public class JavaFXRummikub extends Application {
     private static final int SCENE_WIDTH = 800;
     private static final int SCENE_HEIGHT = 600;
     Stage primaryStage;
-    private Scene newGameScene;
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
 
     @Override
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
-        gamePlayFxmlLoader = getFXMLLoaderByRelativePath(GAME_PLAY_SCENE_FILE_PATH);
-        createNewGameScene();
+        Scene newGameScene = getNewGameScene();
 
         primaryStage.setTitle("Welcome to Rumikub!");
         primaryStage.setResizable(false);
@@ -32,35 +33,57 @@ public class JavaFXRummikub extends Application {
         primaryStage.show();
     }
 
-    private void createNewGameScene() {
+    private Scene getNewGameScene() {
+        Scene newGameScene = null;
         FXMLLoader fxmlLoader = getFXMLLoaderByRelativePath(NEW_GAME_SCENE_FILE_PATH);
         Parent newGameRoot = (Parent) fxmlLoader.getRoot();
         NewGameSceneController newGameSceneController = getNewGameSceneController(fxmlLoader);
         registerGamePlayToStartPlayButton(newGameSceneController);
         if (newGameRoot != null) {
             newGameScene = new Scene(newGameRoot, SCENE_WIDTH, SCENE_HEIGHT);
-        }
-        else {
+        } else {
             //TODO
         }
+        
+        return newGameScene;
+    }
+
+    private NewGameSceneController getNewGameSceneController(FXMLLoader fxmlLoader) {
+        NewGameSceneController controller = (NewGameSceneController) fxmlLoader.getController();
+        return controller;
     }
 
     private void registerGamePlayToStartPlayButton(NewGameSceneController newGameSceneController) {
         newGameSceneController.isStartPlayPressed().addListener((source, oldValue, isFinished) -> {
             if (isFinished) {
-                Scene gamePlayScene = getGamePlayScene();
-                GamePlaySceneController gamePlayConroller = getGamePlaySceneController(gamePlayFxmlLoader);
-                gamePlayConroller.setGame(newGameSceneController.getGame());
-                registerNewGameSceneToMainMenuButton(newGameSceneController, gamePlayConroller);
+                Scene gamePlayScene = getGamePlayScene(newGameSceneController.getGame());
                 primaryStage.setScene(gamePlayScene);
             }
         });
     }
 
-    public static void main(String[] args) {
-        launch(args);
+    private Scene getGamePlayScene(Game game) {
+        FXMLLoader gamePlayFxmlLoader = getFXMLLoaderByRelativePath(GAME_PLAY_SCENE_FILE_PATH);
+        Parent gamePlayRoot = (Parent) gamePlayFxmlLoader.getRoot();
+        GamePlaySceneController gamePlayConroller = getGamePlaySceneController(gamePlayFxmlLoader);
+        gamePlayConroller.setGame(game);
+        registerNewGameSceneToMainMenuButton(gamePlayConroller);
+        return new Scene(gamePlayRoot, SCENE_WIDTH, SCENE_HEIGHT);
+    }
+    
+    private GamePlaySceneController getGamePlaySceneController(FXMLLoader fxmlLoader) {
+        GamePlaySceneController controller = (GamePlaySceneController) fxmlLoader.getController();
+        return controller;
     }
 
+    private void registerNewGameSceneToMainMenuButton(GamePlaySceneController gamePlayConroller) {
+        gamePlayConroller.IsMainMenuButtonPressed().addListener((source, oldValue, isExitToMainMenu) -> {
+            if (isExitToMainMenu) {
+                primaryStage.setScene(getNewGameScene());
+            }
+        });
+    }
+    
     private FXMLLoader getFXMLLoaderByRelativePath(String path) {
         FXMLLoader fxmlLoader = new FXMLLoader();
         URL url = getClass().getResource(path);
@@ -72,28 +95,4 @@ public class JavaFXRummikub extends Application {
         }
         return fxmlLoader;
     }
-
-    private NewGameSceneController getNewGameSceneController(FXMLLoader fxmlLoader) {
-        NewGameSceneController controller = (NewGameSceneController) fxmlLoader.getController();
-        return controller;
-    }
-
-    private GamePlaySceneController getGamePlaySceneController(FXMLLoader fxmlLoader) {
-        GamePlaySceneController controller = (GamePlaySceneController) fxmlLoader.getController();
-        return controller;
-    }
-
-    private void registerNewGameSceneToMainMenuButton(NewGameSceneController newGameSceneController, GamePlaySceneController gamePlayConroller) {
-        gamePlayConroller.IsMainMenuButtonPressed().addListener((source, oldValue, isExitToMainMenu) -> {
-            if (isExitToMainMenu) {
-                primaryStage.setScene(newGameScene);
-            }
-        });
-    }
-
-    private Scene getGamePlayScene() {
-        Parent gamePlayRoot = (Parent) gamePlayFxmlLoader.getRoot();
-        return new Scene(gamePlayRoot, SCENE_WIDTH, SCENE_HEIGHT);
-    }
-    private FXMLLoader gamePlayFxmlLoader;
 }
