e083f0abc0b8b22752077151cb35c9662ff5b180
LiorHalfon <lior.halfon2@gmail.com>
2016-01-13 21:02:13 +0200

    New Game Creation added.

--- JavaFXRummikub/src/javafxrummikub/scenes/newGame/NewGameSceneController.java
+++ JavaFXRummikub/src/javafxrummikub/scenes/newGame/NewGameSceneController.java
@@ -9,28 +9,21 @@ import javafx.beans.binding.BooleanBinding;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.value.ObservableValue;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleGroup;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
-import logic.Game;
-import logic.GameDetails;
-import logic.PlayerDetails;
 import logic.persistency.FileDetails;
 import logic.persistency.GamePersistency;
+import ws.rummikub.DuplicateGameName_Exception;
+import ws.rummikub.InvalidParameters_Exception;
 import ws.rummikub.RummikubWebService;
 
 public class NewGameSceneController implements Initializable {
@@ -50,33 +43,23 @@ public class NewGameSceneController implements Initializable {
     @FXML
     private Label errorMsgLabel;
     @FXML
-    private TableView<PlayerDetails> playersTable;
-    @FXML
-    private TableColumn<PlayerDetails, String> playerNameColumn;
-    @FXML
-    private TableColumn<PlayerDetails, Boolean> isHumanColumn;
-    @FXML
-    private Button addNewPlayerButton;
-    @FXML
-    private TextField newPlayerName;
-    @FXML
-    private CheckBox newPlayerIsHuman;
-    @FXML
     private TextField newGameName;
+    @FXML
+    private TextField humanPlayersNumText;
+    @FXML
+    private TextField computerPlayersNumText;
 
-    private ObservableList<PlayerDetails> playersInputData;
     private static int CURRENT_PLAYER_ID = 1;
     private static final int MAX_PLAYERS_NUMBER = 4;
     private static final int MIN_PLAYERS_NUMBER = 2;
     private static final int MIN_HUMAN_PLAYERS_NUMBER = 1;
-    private Game game;
     private ToggleGroup newGameOptions;
     private SimpleBooleanProperty backToGamesListScene;
     private boolean isPlayersFormInitialized;
     private SimpleBooleanProperty gameLoadedSuccessfully;
     private SimpleBooleanProperty newGameFormValid;
     private SimpleBooleanProperty gameNameValid;
-    private SimpleIntegerProperty currNewPlayersNum;
+    private SimpleIntegerProperty totalPlayersNum;
     private SimpleIntegerProperty humanPlayersNum;
     private RummikubWebService server;
 
@@ -90,17 +73,47 @@ public class NewGameSceneController implements Initializable {
     @FXML
     private void onOkButtonPressed(ActionEvent event) {
         if (newGameOptions.getSelectedToggle() == createNewGameBtn) {
-            GameDetails gameDetails = new GameDetails(newGameName.getText(), playersInputData, null);
-            game = new Game(gameDetails);
-            game.reset();
+            try {
+                server.createGame(
+                        newGameName.getText(),
+                        humanPlayersNum.get(),
+                        totalPlayersNum.get() - humanPlayersNum.get());
+            } catch (DuplicateGameName_Exception | InvalidParameters_Exception ex) {
+                errorMsgLabel.setText(ex.getMessage());
+                return;
+            }
         }
 
         backToGamesListScene.set(true);
     }
-    
+
     @FXML
     private void onBackButtonPressed(ActionEvent event) {
         backToGamesListScene.set(true);
+    }
+
+    @FXML
+    private void onNewPlayerNumTyped() {
+        clearErrorMsg();
+        String humanNum = humanPlayersNumText.getText();
+        if (!humanNum.isEmpty()) {
+            try {
+                humanPlayersNum.set(Integer.parseInt(humanNum));
+            } catch (NumberFormatException numberFormatException) {
+                humanPlayersNum.set(0);
+            }
+        }
+
+        String compNumStr = computerPlayersNumText.getText();
+        int compNum = 0;
+        if (!compNumStr.isEmpty()) {
+            try {
+                compNum = Integer.parseInt(compNumStr);
+            } catch (NumberFormatException numberFormatException) {
+                compNum = 0;
+            }
+        }
+        totalPlayersNum.set(compNum + humanPlayersNum.get());
     }
 
     public SimpleBooleanProperty isBackToGamesListScene() {
@@ -114,7 +127,7 @@ public class NewGameSceneController implements Initializable {
         gameLoadedSuccessfully = new SimpleBooleanProperty(false);
         newGameFormValid = new SimpleBooleanProperty(false);
         gameNameValid = new SimpleBooleanProperty(false);
-        currNewPlayersNum = new SimpleIntegerProperty(0);
+        totalPlayersNum = new SimpleIntegerProperty(0);
         humanPlayersNum = new SimpleIntegerProperty(0);
         newGameName.textProperty().addListener((ObservableValue<? extends String> observableValue, String s, String s2) -> {
             boolean isEmpty = newGameName.textProperty().get().isEmpty();
@@ -125,15 +138,17 @@ public class NewGameSceneController implements Initializable {
                 gameNameValid.set(false);
             }
         });
-        BooleanBinding playersBinding = Bindings.and(
-                currNewPlayersNum.greaterThanOrEqualTo(MIN_PLAYERS_NUMBER),
-                humanPlayersNum.greaterThanOrEqualTo(MIN_HUMAN_PLAYERS_NUMBER));
+        BooleanBinding playersBinding = Bindings.and( Bindings.and(
+                totalPlayersNum.greaterThanOrEqualTo(MIN_PLAYERS_NUMBER),
+                humanPlayersNum.greaterThanOrEqualTo(MIN_HUMAN_PLAYERS_NUMBER)),
+                totalPlayersNum.lessThanOrEqualTo(MAX_PLAYERS_NUMBER));
+        
         BooleanBinding newGameFormBindings = Bindings.and(playersBinding, gameNameValid);
         newGameFormValid.bind(newGameFormBindings);
         newGameOptions.selectedToggleProperty().addListener((ObservableValue<? extends Toggle> ov, Toggle old_toggle, Toggle new_toggle) -> {
             clearErrorMsg();
             gameLoadedSuccessfully.set(false);
-            currNewPlayersNum.set(0);
+            totalPlayersNum.set(0);
             if (newGameOptions.getSelectedToggle() == loadGameFromFileBtn) {
                 handleLoadGameFromFile();
             } else if (newGameOptions.getSelectedToggle() == createNewGameBtn) {
@@ -173,32 +188,7 @@ public class NewGameSceneController implements Initializable {
         }
     }
 
-    @FXML
-    private void addNewPlayer(ActionEvent event) {
-        boolean isHuman = newPlayerIsHuman.isSelected();
-        String playerName = newPlayerName.getText();
-        if (!isPlayerNameExist(playerName)) {
-            playersInputData.add(new PlayerDetails(CURRENT_PLAYER_ID, playerName, isHuman));
-            CURRENT_PLAYER_ID++;
-            currNewPlayersNum.setValue(currNewPlayersNum.getValue() + 1);
-            if (isHuman) {
-                humanPlayersNum.setValue(humanPlayersNum.getValue() + 1);
-            }
-
-            if (CURRENT_PLAYER_ID > MAX_PLAYERS_NUMBER) {
-                addNewPlayerButton.disableProperty().set(true);
-            }
-            newPlayerName.clear();
-        } else {
-            errorMsgLabel.setText("Player name \"" + playerName + "\" is already exist");
-        }
-    }
-
     private void initNewGameForm() {
-        playersInputData = FXCollections.observableArrayList();
-        playerNameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
-        isHumanColumn.setCellValueFactory(new PropertyValueFactory<>("isHuman"));
-        playersTable.setItems(playersInputData);
         isPlayersFormInitialized = true;
     }
 
@@ -237,20 +227,10 @@ public class NewGameSceneController implements Initializable {
         errorMsgLabel.setText("");
     }
 
-    public Game getGame() {
-        return game;
-    }
-
-    private boolean isPlayerNameExist(String playerName) {
-        return playersInputData.stream().anyMatch((player) -> (player.getName().toLowerCase().equals(playerName.toLowerCase())));
-    }
-
     private void clearNewGameForm() {
-        playersInputData.clear();
-        newPlayerName.clear();
         newGameName.clear();
-        CURRENT_PLAYER_ID = 1;
-        addNewPlayerButton.disableProperty().set(false);
+        humanPlayersNumText.clear();
+        computerPlayersNumText.clear();
     }
 
     private void initCreateGameButton() {
