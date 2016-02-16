c13e0dd35040c82ff4c976886b1d40ecc8b358d9
LiorHalfon <lior.halfon2@gmail.com>
2016-02-16 20:02:59 +0200

    Create Game Servlet added

--- RummikubWebClient/src/java/servlets/joingame/CreateGameServlet.java
+++ RummikubWebClient/src/java/servlets/joingame/CreateGameServlet.java
@@ -2,32 +2,45 @@ package servlets.joingame;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import servlets.utils.ServletUtils;
+import ws.rummikub.DuplicateGameName_Exception;
+import ws.rummikub.InvalidParameters_Exception;
 import ws.rummikub.RummikubWebService;
 
-// Will return a list of gameDetails to show in the waiting games table.
-@WebServlet(name = "WaitingGamesServlet", urlPatterns = {"/updatewaiting"})
-public class WaitingGamesServlet extends HttpServlet {
+@WebServlet(name = "CreateGameServlet", urlPatterns = {"/createGame"})
+public class CreateGameServlet extends HttpServlet {
 
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        PrintWriter writer = response.getWriter();
-        RummikubWebService webService = ServletUtils.getWebService(getServletContext());
-        if (webService == null){
-            writer.write("Could not connect to the server at this time.");
-            return;
+        try (PrintWriter out = response.getWriter()) {
+            RummikubWebService ws = ServletUtils.getWebService(getServletContext());
+            try {
+                ws.createGame("Test Game", 2, 1);
+            } catch (DuplicateGameName_Exception | InvalidParameters_Exception ex) {
+                out.println(ex.getMessage());
+                return;
+            }
+            out.println("Game 'Test Game' created seccessfully");
         }
-        List<String> waitingGames = webService.getWaitingGames();
-        waitingGames.forEach((s) -> writer.println(s));
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods.">
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
      *
