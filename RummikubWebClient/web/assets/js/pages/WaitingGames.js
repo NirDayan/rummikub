eea0a3d0480a8cc7a03578fc338f569e786c3ba3
Nir Dayan <nir.dayan@sap.com>
2016-02-18 23:40:38 +0200

    WaitingGames page: add fields and validations

--- RummikubWebClient/web/assets/js/pages/WaitingGames.js
+++ RummikubWebClient/web/assets/js/pages/WaitingGames.js
@@ -6,6 +6,7 @@ define([
     function WaitingGames() {
         this.pollingInterval = null;
         this.currentWaitingGames = null;
+        this.selectedTableRow = null;
     }
 
     WaitingGames.prototype = {
@@ -14,6 +15,7 @@ define([
             var namesString = "";
             var namesList;
 
+            this.selectedTableRow = null;
             for (var i = 0; i < waitingGames.length; i++) {
                 namesList = waitingGames[i].unjoinedPlayersNames;
                 for (var j = 0; j < namesList.length; j++) {
@@ -30,14 +32,33 @@ define([
                         "<td>" + namesString + "</td>");
 
             }
+            this.updateSelectedTableRow(tableBody);
         },
-        initialize: function () {
+        updateSelectedTableRow: function (tableBody) {
+            var that = this;
+            tableBody.find('tr').on("click", function () {
+                var clickedRow = jQuery(this);
+                if (clickedRow.hasClass("selected")) {
+                    clickedRow.removeClass("selected");
+                    that.selectedTableRow = null;
+                } else {
+                    tableBody.find('tr').removeClass("selected");
+                    clickedRow.addClass("selected");
+                    that.selectedTableRow = clickedRow;
+                }
+                jQuery("#playerNameInput").trigger("keyup");
+            });
+        },
+        startPolling: function () {
             this.pollingInterval = setInterval(function () {
-                var waitingGamesRequest = jQuery.ajax({
+                var waitingGamesRequest = jQuery.get({
                     url: "./waitingGames"
                 });
 
                 waitingGamesRequest.done(function (waitingGames) {
+                    if (!waitingGames || waitingGames.length === 0) {
+                        return;
+                    }
                     if (this.currentWaitingGames && _.isEqual(this.currentWaitingGames, waitingGames)) {
                         //List has not been changed. Do Nothing...
                         return;
@@ -51,6 +72,39 @@ define([
                     return;
                 });
             }.bind(this), 1000);
+        },
+        initPlayerNameField: function () {
+            var that = this;
+            jQuery("#playerNameInput").on("keyup", function () {
+                if (jQuery(this).val().trim().length > 2 && that.selectedTableRow != null) {
+                    jQuery("#joinGameButton").attr("disabled", false);
+                } else {
+                    jQuery("#joinGameButton").attr("disabled", true);
+                }
+            });
+        },
+        initJoinGameButton: function () {
+            jQuery("#joinGameButton").on("click", function () {
+                var gameName = this.selectedTableRow.find("td:first").text();
+                var playerName = jQuery("#playerNameInput").val();
+                if (gameName && playerName) {
+                    jQuery.post("./joinGame", {
+                        gameName: gameName,
+                        playerName: playerName
+                    }).done(function () {
+                        //Todo: continue...
+                    }).fail(function () {
+                        //TODO: error message
+                    });
+                } else {
+                    //TODO: error message
+                }
+            }.bind(this));
+        },
+        initialize: function () {
+            this.startPolling();
+            this.initPlayerNameField();
+            this.initJoinGameButton();
         },
         close: function () {
             if (this.pollingInterval) {
