1fa27fe39bde2138b3d7db4d6fd74a36b48c24a2
LiorHalfon <lior.halfon2@gmail.com>
2016-02-20 17:41:12 +0200

    Fixed Typo, and cleaned unused constants

--- RummikubWebClient/src/java/servlets/joingame/LoadGameFormXml.java
+++ RummikubWebClient/src/java/servlets/joingame/LoadGameFormXml.java
@@ -1,18 +1,11 @@
 package servlets.joingame;
 
-import com.google.gson.JsonObject;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintWriter;
-import java.util.Collection;
-import java.util.Scanner;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Part;
-import servlets.utils.ServletConstants;
 import servlets.utils.ServletUtils;
 
 // This servlet returns a json that spcifie if the load was successful or not
--- RummikubWebClient/src/java/servlets/utils/ServletConstants.java
+++ RummikubWebClient/src/java/servlets/utils/ServletConstants.java
@@ -2,6 +2,4 @@ package servlets.utils;
 
 public class ServletConstants {
     public static final String PLAYER_ID = "playerId";
-    public static final String IS_SUCCESS = "isSuccess";
-    public static final String ERROR_MSG = "errorMsg";
 }
